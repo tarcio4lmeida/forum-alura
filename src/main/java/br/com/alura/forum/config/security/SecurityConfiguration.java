c99124549462122b906c27d695b541ff7a4e9996
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-04-17 19:20:08 -0300

    restrigindo o acesso aos endpoints privados

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -23,7 +23,10 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
+		
 	}
 	
 	// Configuracoes de Recursos estatiscos (js, css, imagens, etc)
