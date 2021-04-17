7ebf69557b3066b246c65245d52fa2c060e47bf1
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-04-17 20:47:15 -0300

    Gerando Tokens JWT

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$PA3E4KSQ2V9aAMmCgQs3Z.bCw6RNyuoyYsuLyH0waXBMawD0bKrVe
-');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$PA3E4KSQ2V9aAMmCgQs3Z.bCw6RNyuoyYsuLyH0waXBMawD0bKrVe');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
