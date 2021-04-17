f57ce7707aae23c0043794d9726aba289fbb9ceb
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-04-17 19:47:43 -0300

    Autenticando o usuário

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$PA3E4KSQ2V9aAMmCgQs3Z.bCw6RNyuoyYsuLyH0waXBMawD0bKrVe
+');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
