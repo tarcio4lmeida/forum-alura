b6ae36b3edcd83c5b6948065e2120c4b6925d898
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-04-17 18:35:46 -0300

    Invalidando o cache

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -62,6 +63,7 @@ public class TopicosController {
 	// 201 -> Criado 
 	@PostMapping
 	@Transactional
+	@CacheEvict(value="listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -82,6 +84,7 @@ public class TopicosController {
 	//pequena atualizacao
 	@PutMapping ("/{id}")
 	@Transactional // Efetuar o commit automático da transação, caso não ocorra uma exception
+	@CacheEvict(value="listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDTO> atualizar(@PathVariable("id") Long id, @RequestBody  @Valid AtualizacaoTopicoForm form) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if(optional.isPresent()) {
@@ -93,6 +96,7 @@ public class TopicosController {
 	
 	@DeleteMapping ("/{id}")
 	@Transactional // Efetuar o commit automático da transação, caso não ocorra uma exception
+	@CacheEvict(value="listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable("id") Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if(optional.isPresent()) {
