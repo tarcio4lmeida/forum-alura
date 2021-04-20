b78d87b0fdd9fda76e26d9f4231d4555661a2cb9
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-04-20 11:04:30 -0300

    Testes automatizados com Spring

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -20,7 +20,7 @@ import br.com.alura.forum.dto.TokenDTO;
 
 @RestController
 @RequestMapping("/auth")
-@Profile("prod")
+@Profile(value={"prod", "test"})
 public class AutenticacaoController {
 	@Autowired
 	private AuthenticationManager authManager;
--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,25 +12,26 @@ import br.com.alura.forum.modelo.Curso;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CursoRepositoryTest {
-	
+
 	@Autowired
 	private CursoRepository repository;
-	
+
 	@Test
 	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
 		String nomeCurso = "HTML 5";
 		Curso curso = repository.findByNome(nomeCurso);
-		
+
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(nomeCurso, curso.getNome());
 	}
-	
+
 	@Test
 	public void deveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
 		String nomeCurso = "JPA";
 		Curso curso = repository.findByNome(nomeCurso);
-		
+
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(nomeCurso, curso.getNome());
 	}
